75c552901d75af22cf63e8324b01e6473cf26150
christianbenson <Christianmbenson@gmail.com>
2023-04-04 10:20:45 -0500

    first commit

---
+++ CBMenu.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+public class MenuItem
+{
+    public string Appitizer (get; set;)
+     public string MainCourse (get; set;)
+      public string Dessert (get; set;)
+      public bool New {get; set; } = true
+}
+public class program
+{
+    public static void Main(string[] args)
+    {
+        MenuItem order = new MenuItem();
+       
+       
+        order.Appitizer = "Burnt Ends";
+        order.MainCourse = "Grilled Salmon and Rice";
+        order.Dessert = "The Heartbreak";
+        
+
+
+       Console.WriteLine("Appitizer\n" + order.Appitizer + ": $5.99\n   Enjoy these burnt ends that will have you question if you turned the oven off at home!");
+       Console.WriteLine("\nMainCourse\n" + order.MainCourse + " : $15.99   NEW\n         Fresh salmon sourced locally with a seasoned rice with kickin' flavor it makes you savor the flavor! \n");
+       Console.WriteLine("\nDessert\n" + order.Dessert + " :$18.90\n    The Heartbreak is a house special that is made with goat milk that will have you sound like a New York Native, Forgetta bout her.");
+    }
+}
+
+
+
+
+
